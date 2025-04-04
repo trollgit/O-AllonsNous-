792f8b55144909d201461f5e9d967f2ed04fe47d
trollgit <davidemichigan@gmail.com>
2025-04-04 22:45:00 +0200

    Update logout.js

--- java/logout.js
+++ java/logout.js
@@ -1,8 +1,9 @@
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { auth } from "./firebase-config.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
 document.getElementById("logout").addEventListener("click", (e) => {
     e.preventDefault();
-    signOut(getAuth()).then(() => {
+    signOut(auth).then(() => {
         window.location.href = "login.html";
     });
 });
